072253edb76f6303bd522dc50fa2c48eda16d3ab
rochellev <rochellevroberts@utexas.edu>
2021-03-11 15:47:22 -0800

    finish hooking up buttons with gapi

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -23,19 +23,27 @@ class GoogleAuth extends React.Component {
   onAuthChange = () => {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui blue google button">
+        <button className="ui blue google button" onClick={this.onSignOut}>
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button className="ui blue google button">
+        <button className="ui blue google button" onClick={this.onSignIn}>
           <i className="google icon" />
           Sign In with Google
         </button>
