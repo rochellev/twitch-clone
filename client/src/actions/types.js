177d521417484d15b63b83cf5aeab3e329563b0a
rochellev <rochellevroberts@utexas.edu>
2021-03-12 11:07:45 -0800

    add types, reference string through variables

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,4 @@
-export const SIGN_IN = 'SIGN_IN';
-export const SIGN_OUT = 'SIGN_OUT';
+// define all types (strings) used in reducers and action creators
+// no longer refer to plain strings, reduce typo errors
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT = "SIGN_OUT";
