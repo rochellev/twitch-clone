0e0086fa760cd720d666c4ca6d0ee60a4588b35d
rochellev <rochellevroberts@utexas.edu>
2021-03-12 11:04:49 -0800

    move isSignedIn bool to store, so app can access not just the GoogleAuth component

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,13 +1,16 @@
-import { SIGN_IN, SIGN_OUT } from '../actions/types';
+import { SIGN_IN, SIGN_OUT } from "../actions/types";
 
-const INTIAL_STATE = {
+// true constant object -- no modify
+const INITIAL_STATE = {
   isSignedIn: null,
   userId: null
 };
 
-export default (state = INTIAL_STATE, action) => {
+// default argument, to initialize it, make it null
+export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_IN:
+      // create an entirely new object using spread and update some properties
       return { ...state, isSignedIn: true, userId: action.payload };
     case SIGN_OUT:
       return { ...state, isSignedIn: false, userId: null };
