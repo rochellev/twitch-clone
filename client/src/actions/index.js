c1a0e169f5e421a353d3de352e06e427e74e13f2
rochellev <rochellevroberts@utexas.edu>
2021-03-15 14:59:57 -0700

    add action types and import them

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,13 @@
-import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
-
+import streams from "../apis/streams";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
 export const signIn = userId => {
   return {
     type: SIGN_IN,
@@ -13,8 +20,8 @@ export const signOut = () => {
     type: SIGN_OUT
   };
 };
-
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post('/streams', formValues);
-  dispatch({type: CREATE_STREAM, payload: response.data})
-} 
+// following REST-ful api
+export const createStream = formValues => async dispatch => {
+  const response = await streams.post("/streams", formValues);
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
