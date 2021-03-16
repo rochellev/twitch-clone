dcbc24a8685865a724fcdb4325a6ab5bc448cebe
rochellev <rochellevroberts@utexas.edu>
2021-03-16 11:07:27 -0700

    add buttons for edit, delete and create

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -7,7 +7,7 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
-
+  // logic to decide if we should show edit/delete buttons for author of a given stream
   renderAdmin(stream) {
     if (stream.userId === this.props.currentUserId) {
       return (
@@ -36,7 +36,9 @@ class StreamList extends React.Component {
     });
   }
 
+  // helper to show the edit button (LINK since nav to page)
   renderCreate() {
+    // can use currentUserId but have property isSignedIn already in auth object
     if (this.props.isSignedIn) {
       return (
         <div style={{ textAlign: "right" }}>
